069c853db5561a0f5faa5c06d10ff6ef7dbf3562
Антон Садаков <sadacov_anton@mail.ru>
2023-12-07 19:35:28 +0500

    3 р. На проверку

--- build/js/modal.js
+++
@@ -1,1 +0,0 @@
-let modal=document.getElementById("myModal"),btn=document.getElementById("myBtn");btn.onclick=function(){modal.style.display="block"},window.onclick=function(n){n.target==modal&&(modal.style.display="none")};
